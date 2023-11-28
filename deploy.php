97a7c639964dbb63304d24d96ffcd9fd65f82bfa
Pongpetch <pongpet007@gmail.com>
2023-11-28 09:12:40 +0700

    add deploy

---
+++ deploy.php
@@ -0,0 +1,31 @@
+<?php
+
+
+    $commands = array(
+        "echo $PWD",
+        "whoami",
+        "git pull",
+        "git status",
+        "git submodule sync",
+        "git submodule update",
+        "git submodule status",
+    );
+    $output = '';
+    foreach($commands as $command){
+        $tmp = shell_exec($command);
+        $output .="<span style=\"color:black;\">\${$command}</span><br>";
+        $output .= htmlentities(trim($tmp))."<br><br>";
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
