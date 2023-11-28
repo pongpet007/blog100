2f48a7ea51a470efb897cf700ced5fdae6096d1a
Pongpetch <pongpet007@gmail.com>
2023-11-28 09:57:02 +0700

    add 4

--- deploy.php
+++ deploy.php
@@ -12,7 +12,8 @@
     );
     $output = '';
     foreach($commands as $command){
-        $tmp = shell_exec($command);
+        // $tmp = shell_exec($command);
+        $tmp = exec($command);
         $output .="<span style=\"color:black;\">\${$command}</span><br>";
         $output .= htmlentities(trim($tmp))."<br><br>";
     }
