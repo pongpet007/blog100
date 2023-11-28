18a03ccf8382598cafe10f64d2178f6eee905d10
Pongpetch <pongpet007@gmail.com>
2023-11-28 11:56:36 +0700

    restart project

--- deploy3.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$cmd = shell_exec("git pull 2>&1");
-
-#for debugging
-echo $cmd;
-
-?>
--- my-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /home/brand100/domains/aboutblog.in.th/public_html  && \
-git pull origin main
-
-cd /home/brand100/domains/bangkokclassified.in.th/public_html  && \
-git pull origin main
