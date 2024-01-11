b494a1fc4107a184da8cb329f6a463389ed32b5b
pongpet007 <pongpet007@gmail.com>
2024-01-11 15:36:10 +0700

    edit

--- admin/application/controllers/CompanyBrand.php
+++ admin/application/controllers/CompanyBrand.php
@@ -5,7 +5,7 @@ class CompanyBrand extends CI_Controller {
 
 
 	function __construct(){
-						
+
 		parent::__construct();
 		$this->load->model('Menu_admin_model');	
 		$this->load->model('Menu_admin_type_model');	
@@ -47,18 +47,18 @@ class CompanyBrand extends CI_Controller {
 	}
 
 	public function add(){					
-			
+
 		$params = array('com_id'=> 1,		
-						'is_active'=>1,									
-						'uby'=>$this->session->userdata('ssfullname'),
-						'cdate'=> date('Y-m-d H:i:s'),
-						'udate'=> date('Y-m-d H:i:s')
-						 );
+			'is_active'=>1,									
+			'uby'=>$this->session->userdata('ssfullname'),
+			'cdate'=> date('Y-m-d H:i:s'),
+			'udate'=> date('Y-m-d H:i:s')
+		);
 		
 		$this->db->insert('company_brand',$params);
 
 		$brand_id = $this->db->insert_id();
-				
+
 		$params = array('position'=>$brand_id);
 
 		$this->db->where('brand_id', $brand_id);
@@ -69,90 +69,133 @@ class CompanyBrand extends CI_Controller {
 
 		foreach ($country as  $countrynew) {
 			$params = array(
-					'brand_id'=>$brand_id,
-					'country_id'=>$countrynew->country_id
+				'brand_id'=>$brand_id,
+				'country_id'=>$countrynew->country_id
 			);
 			$this->db->insert('company_brand_language',$params);
 		}
 
 		redirect("CompanyBrand/edit/$brand_id");			
-	
+
 	}
 
 	public function edit($brand_id){
 		$com_id = 1;
 		$this->form_validation->set_rules('btn_send','btn_send','trim|required');
 
-			$keyword = $this->input->post('keyword');
+		$keyword = $this->input->post('keyword');
 
-			if($this->form_validation->run()==FALSE or !empty($keyword)){
+		if($this->form_validation->run()==FALSE or !empty($keyword)){
 				//if($this->input->post('username')){
 				//////////////////// Menu /////////////////////////////////
-				$menu_types = $this->Menu_admin_type_model->getAll();
-				foreach ($menu_types as $menu_type) {
-					$menu_type->menu = $this->Menu_admin_model->getAll($menu_type->menu_admin_type_id);
-				}
+			$menu_types = $this->Menu_admin_type_model->getAll();
+			foreach ($menu_types as $menu_type) {
+				$menu_type->menu = $this->Menu_admin_model->getAll($menu_type->menu_admin_type_id);
+			}
 
-				$data['menu_admins'] =  $menu_types;
-				$menuselected = $this->input->get('menuselected');
-				if($menuselected > 0){
-					$data['menuselected'] =  $this->Menu_admin_model->getOne($menuselected);				
-				}
+			$data['menu_admins'] =  $menu_types;
+			$menuselected = $this->input->get('menuselected');
+			if($menuselected > 0){
+				$data['menuselected'] =  $this->Menu_admin_model->getOne($menuselected);				
+			}
 				############################################################
-				$this->session->set_flashdata('errors', validation_errors());
+			$this->session->set_flashdata('errors', validation_errors());
 
 				//////////////////////////// Language ////////////////////////////
-				$countrys = $this->Country_model->getAll();		
-				$arr = array('0'=>' -- All Country -- ');
-				foreach ($countrys as $key => $value) {
-					$arr[$value->country_id] = $value->short_name;
-				}
-				$data['countrys'] = $arr ;
+			$countrys = $this->Country_model->getAll();		
+			$arr = array('0'=>' -- All Country -- ');
+			foreach ($countrys as $key => $value) {
+				$arr[$value->country_id] = $value->short_name;
+			}
+			$data['countrys'] = $arr ;
 
-				$countrys = $this->Country_model->getAll();					
-				foreach ($countrys as $country) {
-					$country->brand_languages = $this->Company_brand_language_model->getAll(array('country_id'=>$country->country_id,'brand_id'=>$brand_id));									
-				}			
-				$data['languages'] = $countrys ;
+			$countrys = $this->Country_model->getAll();					
+			foreach ($countrys as $country) {
+				$country->brand_languages = $this->Company_brand_language_model->getAll(array('country_id'=>$country->country_id,'brand_id'=>$brand_id));									
+			}			
+			$data['languages'] = $countrys ;
 
 
-				
-				$data["com_id"] = $com_id ;				
-				
-				$data["method"] = 'edit' ;
-								
-				$data['brand_id'] =$brand_id;
-				
-				$data['contentview'] = 'pagecompany/brand_form';	
-				
-				$data['keyword'] = $keyword;
 
-				$data['company'] = $this->Company_model->getOne($com_id);
-				$data['brand'] = $this->Company_brand_model->getOne($brand_id);				
-				$this->load->view('layoutcompany/main',$data);				
+			$data["com_id"] = $com_id ;				
+
+			$data["method"] = 'edit' ;
+
+			$data['brand_id'] =$brand_id;
+
+			$data['contentview'] = 'pagecompany/brand_form';	
+
+			$data['keyword'] = $keyword;
+
+			$data['company'] = $this->Company_model->getOne($com_id);
+			$data['brand'] = $this->Company_brand_model->getOne($brand_id);				
+			$this->load->view('layoutcompany/main',$data);				
 			
 		}else{
 
 			
-				$is_active = $this->input->post('is_active');
-				$position = $this->input->post('position');	
+			$is_active = $this->input->post('is_active');
+			$position = $this->input->post('position');	
 
-				$start_date = $this->input->post('start_date');
-				$end_date = $this->input->post('end_date');				
-				$params = array('position'=>$position,
-								'is_active'=>$is_active,
-								'start_date'=>$start_date,
-								'end_date'=>$end_date,							
-								'uby'=>$this->session->userdata('ssfullname'),
-								'udate'=> date('Y-m-d H:i:s') );
+			$start_date = $this->input->post('start_date');
+			$end_date = $this->input->post('end_date');				
+			$params = array('position'=>$position,
+				'is_active'=>$is_active,
+				'start_date'=>$start_date,
+				'end_date'=>$end_date,							
+				'uby'=>$this->session->userdata('ssfullname'),
+				'udate'=> date('Y-m-d H:i:s') );
 
 			$this->db->where('brand_id', $brand_id);
 			$this->db->update('company_brand',$params);
 			
-		
+
 			if(isset($_FILES['picture'])&&$_FILES['picture']['size']>0){
 				$filepath = "../images/brand/{$brand_id}.jpg";
 				move_uploaded_file($_FILES['picture']['tmp_name'], $filepath);
+			}
+
+			if ($_FILES['picture']['size']>0) {
+				$this->load->library('ftp');
+
+				$config['hostname'] = '27.254.96.231';
+				$config['username'] = 'brand100';
+				$config['password'] = 'Bra1212312121!@#$%^';
+				$config['debug']        = TRUE;
+
+				$domainname = $_SERVER['SERVER_NAME'];
+				preg_match('/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/', $domainname , $matches);     
+				$domainsnow = $matches[1];
+				$domainsold = $this->Company_model->getDomainbyALLS($domainsnow);
+        		// print_r($this->db->last_query());exit();
+				foreach ($domainsold as $key => $new_domains) {
+					$domains[] = $new_domains->com_website;
+				}
+
+				$this->ftp->connect($config);
+
+				$filename = $brand_id.'.jpg';
+				$filename1 = '../images/brand/'.$brand_id.'.jpg';
+
+				foreach ($domains as $domain) {
+					$folder = "/domains/$domain/public_html/images/";
+					if ($this->ftp->list_files($folder) === FALSE) {
+						$this->ftp->mkdir($folder);
+						$this->ftp->chmod($folder, 0777);
+					}
+
+					$folder = "/domains/$domain/public_html/images/brand/";
+					if ($this->ftp->list_files($folder) === FALSE) {
+						$this->ftp->mkdir($folder);
+						$this->ftp->chmod($folder, 0777);
+					}
+            // $_FILES['picture']["tmp_name"];
+					$this->ftp->upload($filename1, $folder.$filename);
+				}
+
+				$this->ftp->close();
+
+				echo "transfer complete";
 			}
 
 			$this->session->set_flashdata('msg','Edit Complete');
@@ -205,7 +248,7 @@ class CompanyBrand extends CI_Controller {
 			$params['cby'] = $this->session->userdata('ssfullname');
 			$params['uby'] = $this->session->userdata('ssfullname');
 			if($country_id > 0)
-			$this->db->insert('company_brand_language',$params);
+				$this->db->insert('company_brand_language',$params);
 		}
 
 		if($languagemethod=='edit'){
@@ -239,6 +282,6 @@ class CompanyBrand extends CI_Controller {
 
 	
 }
- 
+
 
 ?>
